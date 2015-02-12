cec4ec65988079f3f0948bc0255b98e290cac79f
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2015-02-12 03:25:40 -0500

    rootdir: Remove vibrator.ko insmod
    Change-Id: I466219a762d4554d40b59d798dabe127a8263a94

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -31,10 +31,10 @@ BOARD_FAMILY := quincy
 -include device/samsung/msm8660-common/BoardConfigCommon.mk
 
 # Kernel
-BOARD_KERNEL_CMDLINE        := androidboot.hardware=qcom usb_id_pin_rework=true no_console_suspend=true zcache
-BOARD_KERNEL_BASE           := 0x48000000
-BOARD_KERNEL_PAGESIZE       := 2048
-BOARD_MKBOOTIMG_ARGS        := --ramdisk_offset 0x01600000
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom usb_id_pin_rework=true no_console_suspend=true zcache androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x48000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 
 BOARD_HAS_NO_SELECT_BUTTON := true
 
